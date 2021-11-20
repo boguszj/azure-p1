091109a000b570e3d9b8c667342c74425471a42a
Jakub Bogusz <jbogusz@MacBook-Pro-Jakub.local>
2021-11-20 19:42:43 +0100

    Min -> Max

--- p1be/src/main/java/com/azurep1/p1be/report/domain/CreateReportDto.java
+++ p1be/src/main/java/com/azurep1/p1be/report/domain/CreateReportDto.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
@@ -21,7 +18,7 @@ public class CreateReportDto {
 
     @NotNull
     @Min(5)
-    @Min(60)
+    @Max(60)
     private Long intervalSeconds;
 
 }
