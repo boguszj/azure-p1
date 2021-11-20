8944be9bb822e4f3dd63a66bc58041fa355388a3
Jakub Bogusz <jbogusz@MacBook-Pro-Jakub.local>
2021-11-20 19:31:48 +0100

    Fix missing component in spring ioc

--- p1be/src/main/java/com/azurep1/p1be/limitation/domain/PeriodGreaterThanLimitationValidator.java
+++ p1be/src/main/java/com/azurep1/p1be/limitation/domain/PeriodGreaterThanLimitationValidator.java
@@ -1,8 +1,11 @@
 package com.azurep1.p1be.limitation.domain;
 
+import org.springframework.stereotype.Component;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+@Component
 public class PeriodGreaterThanLimitationValidator implements ConstraintValidator<PeriodGreaterThanLimitation, PeriodAndLimitationDto> {
 
    public boolean isValid(PeriodAndLimitationDto dto, ConstraintValidatorContext context) {
